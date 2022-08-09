8bdc9699d3873ce33b0a7a3efcd2256b8fb09cdf
jimmyceroneii <jimmy.cerone@atomicobject.com>
2022-08-08 22:26:32 -0700

    add in randomization on reset and for start

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Comparison } from './Components/Comparison';
 import { ResultDisplay } from './Components/ResultDisplay';
+import { randomizeList } from './helpers/listUtils';
 import { indianaRestaurants } from './restaurantLists';
 
 export const App: React.FC = () => {
-  const initialList1 = indianaRestaurants.slice(
-    0,
-    Math.ceil(indianaRestaurants.length / 2)
+  const [randomizedList, setRandomizedList] = useState(
+    randomizeList({ list: indianaRestaurants })
   );
 
-  const initialList2 = indianaRestaurants.slice(
-    Math.ceil(indianaRestaurants.length / 2),
-    indianaRestaurants.length
+  const [list1, setList1] = useState(
+    randomizedList.slice(
+      Math.ceil(randomizedList.length / 2),
+      randomizedList.length
+    )
   );
 
-  const [list1, setList1] = useState(initialList1);
-
-  const [list2, setList2] = useState(initialList2);
+  const [list2, setList2] = useState(
+    randomizedList.slice(0, Math.ceil(randomizedList.length / 2))
+  );
 
   return (
     <div>
@@ -29,12 +31,20 @@ export const App: React.FC = () => {
         />
       ) : (
         <ResultDisplay
-          initialList1={initialList1}
-          initialList2={initialList2}
+          randomizeList={() => {
+            setRandomizedList(randomizeList({ list: indianaRestaurants }));
+            setList1(
+              randomizedList.slice(
+                Math.ceil(randomizedList.length / 2),
+                randomizedList.length
+              )
+            );
+            setList2(
+              randomizedList.slice(0, Math.ceil(randomizedList.length / 2))
+            );
+          }}
           list1={list1}
           list2={list2}
-          setList1={setList1}
-          setList2={setList2}
         />
       )}
     </div>
--- src/Components/ResultDisplay.tsx
+++ src/Components/ResultDisplay.tsx
@@ -2,34 +2,22 @@ import React from 'react';
 import { styles } from './styles';
 
 type ResultProps = {
-  initialList1: Array<string>;
-  initialList2: Array<string>;
+  randomizeList: () => void;
   list1: Array<string>;
   list2: Array<string>;
-  setList1: React.Dispatch<React.SetStateAction<string[]>>;
-  setList2: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 export const ResultDisplay: React.FC<ResultProps> = ({
-  initialList1,
-  initialList2,
+  randomizeList,
   list1,
   list2,
-  setList1,
-  setList2,
 }) => {
   return (
     <div style={styles.card}>
       <p style={styles.choiceText}>
         You have chosen: {list1.length === 0 ? list2[0] : list1[0]}
       </p>
-      <button
-        style={styles.choiceButton}
-        onClick={() => {
-          setList1(initialList1);
-          setList2(initialList2);
-        }}
-      >
+      <button style={styles.choiceButton} onClick={randomizeList}>
         Reset
       </button>
     </div>
